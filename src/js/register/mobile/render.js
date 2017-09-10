78dc284bf6890b2e2d1ee8ca186b58bc4230ac8e
xieyu03 <xieyu33333@gmail.com>
2017-09-10 14:35:26 +0800

    register-info style

--- src/js/register/mobile/render.js
+++ src/js/register/mobile/render.js
@@ -2,11 +2,11 @@ const tpl = (opts = {}) => {
     return `<div id="register-mobile-wrapper" class="register-mobile-wrapper">
         <form id="register-mobile-form" onsubmit="return false">
             <label>
-                <span>手机号</span>
+                <span>手机号： </span>
                 <input id="register-mobile-input" name="mobile" type="text" placeholder="${ opts.mobilePlaceHolder }" valid="present, mobile">
             </label>
             <label>
-                <span>验证</span>
+                <span>验证： </span>
                 <div id="register-verify-wrapper" class="register-verify-wrapper"></div>
             </label>
             <input id="register-mobile-btn" class="disabled" disabled type="submit" value="发 送">
