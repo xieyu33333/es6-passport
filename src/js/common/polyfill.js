41859a733871411efe28b954de56b28a28a7d44c
xieyu03 <xieyu33333@gmail.com>
2017-09-25 14:21:31 +0800

    add mock

--- src/js/common/polyfill.js
+++ src/js/common/polyfill.js
@@ -3,6 +3,7 @@ import "babel-polyfill";
 import 'es6-promise/auto';
 import 'fetch-detector';
 import 'fetch-ie8';
-if (__DEV__) {
-    require('./mock');
-}
+import './mock';
+// if (__DEV__) {
+    //require('./mock');
+// }
